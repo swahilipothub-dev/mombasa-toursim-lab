1aba237773738eb24cabd9a35837e25f165904e4
ismaelaboud <smail.abud@gmail.com>
2025-08-20 12:24:05 +0300

    chore: add vite-plugin-image-optimizer for image optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+
 
 
 export default defineConfig(({ mode }) => ({
@@ -10,6 +12,13 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    ViteImageOptimizer({
+      png: { quality: 80 },
+      jpeg: { quality: 75 },
+      jpg: { quality: 75 },
+      webp: { quality: 75 },
+      avif: { quality: 50 },
+    }),
   ].filter(Boolean),
   resolve: {
     alias: {
